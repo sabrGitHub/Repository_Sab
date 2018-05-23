e0236dad2985f56be00cf9590347044577e391f2
1795891 <1795891@LI-S064-0T11>
2018-05-23 08:59:53 -0400

    code calss Lab

--- src/refactoringExemple/Lab.java
+++ src/refactoringExemple/Lab.java
@@ -10,5 +10,34 @@ package refactoringExemple;
  * @author 1795891
  */
 public class Lab {
+    String idClass;
+    Integer capacity;
+    Integer nbComputers;
+
+    public String getIdClass() {
+        return idClass;
+    }
+
+    public void setIdClass(String idClass) {
+        this.idClass = idClass;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getNbComputers() {
+        return nbComputers;
+    }
+
+    public void setNbComputers(Integer nbComputers) {
+        this.nbComputers = nbComputers;
+    }
     
+   
+   
 }
