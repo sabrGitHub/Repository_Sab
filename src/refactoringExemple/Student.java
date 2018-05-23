0470aa833c105f21bdee409b31986d6e21313af4
1795891 <1795891@LI-S064-0T11>
2018-05-23 09:09:46 -0400

    code calss Student

--- src/refactoringExemple/Student.java
+++ src/refactoringExemple/Student.java
@@ -5,10 +5,41 @@
  */
 package refactoringExemple;
 
+import java.util.Date;
+
 /**
  *
  * @author 1795891
  */
 public class Student {
+    String student_id;
+    String lastName;
+    String firtName;
+    Date dateBirth;
     
+    public Student(String  n,String p, Date d){
+    this.lastName=n;
+    this.firtName=p;
+    this.dateBirth=d;
+    this.student_id=createStudentId(n,p,d);
+    }
+
+    public String createStudentId(String lName, String fName, Date date) {
+        String end =concatDate(date);
+        return lName.substring(0,3)+fName.charAt(0)+end;
+        
+    }
+
+    private String concatDate(Date date) {
+        return date.getYear()+ "-"+date.getMonth();
+    }
+    public String getStudentId(){
+        return student_id;
+    }
+    public void setStudentId(String student_id){
+    this.student_id=student_id;
+    }
+    public void isSupervisedBy(Professor professor){
+            professor.students.add(this);
+    }
 }
