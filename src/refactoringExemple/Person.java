d38e5b0eb5e8cdc87ecbcf04348d40fe6993fd35
1795891 <1795891@LI-S064-0T11>
2018-05-23 09:37:12 -0400

    Class Person rename

---
+++ src/refactoringExemple/Person.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refactoringExemple;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1795891
+ */
+public class Person {
+    int age;
+    Date dateBirth;
+    String firtName;
+    String lastName;
+    
+}
